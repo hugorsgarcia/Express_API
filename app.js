bcda5a958aab0ad05ccb653587cb2d772a5fb6a6
hugorsgarcia <hhugokta@hotmail.com>
2025-06-15 18:43:47 -0300

    Adicionando comentarios

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+// Carrega variáveis de ambiente do arquivo .env
 require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose")
-const contatoRoutes = require("./routes/contatoRoutes")
+const contatoRoutes = require("./routes/contatoRoutes") // Importa as rotas relacionadas a contatos.
 
+// Inicializa a aplicação Express
 const app = express()
+//interpreta o corpo das requisições como JSON
 app.use(express.json())
 
+// Conecta ao MongoDB usando a URL do arquivo de ambiente.
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log("MongoDB Conectado"))
     .catch((err) => console.log("MongoDB mão conectado", err))
 
+// Monta as rotas de contato no caminho raiz '/'
 app.use('/', contatoRoutes)
-const PORT = process.env.PORT
+// Define a porta do servidor, com 3000 como padrão.
+const PORT = process.env.PORT || 3000;
 
+// Sobre o servidor escutando na porta 3000
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
 })
