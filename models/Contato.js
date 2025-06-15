8c50cfa1fca5b7fafb40343b232b11f1023c7399
hugorsgarcia <hhugokta@hotmail.com>
2025-06-15 18:19:45 -0300

    definindo campos de contatos como obrigatorios

--- models/Contato.js
+++ models/Contato.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
+// Define o schema para a coleção 'Contatos'
 const ContatoSchema = new mongoose.Schema(
     {
-        nome: {type: String, required: true},
-        email: {type: String, required: true},
-        telefone: {type: String, required: true} 
+        
+        nome: { type: String, required: [true, "O campo 'nome' é obrigatório."] },
+        
+        email: { type: String, required: [true, "O campo 'email' é obrigatório."] },
+        
+        telefone: { type: String, required: [true, "O campo 'telefone' é obrigatório."] }
     },
     {
         timestamps: true
     }
 )
 
-module.exports = mongoose.model('Contatos', ContatoSchema)
+// Exporta o model 'Contatos' para ser utilizado nas rotas.
+module.exports = mongoose.model('Contatos', ContatoSchema);
